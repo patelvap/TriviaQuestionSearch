38f2d22beb62cd3d0026e0da32f3eba937d5ace3
Vraj Patel <patelvap@gmail.com>
2019-10-20 16:32:05 -0400

    search component and routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom"
 import './App.css';
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
-      results : 'none',
-      
+      results : 'none', //becomes array with returned items from fetch call
+      hasNext : false,
+      hasPrev : false,
+      currPage: 1,
     }
+  }
+
+  render() {
+    return (
+      <Router basename={process.env.PUBLIC_URL}>
+        <div className="App">
+          
+        </div>
+      </Router>
+    )
   }
 }
 
