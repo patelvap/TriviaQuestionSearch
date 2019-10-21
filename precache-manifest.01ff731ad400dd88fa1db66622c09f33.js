ee4f04c9cf223fa137f807d365001883bc733d93
root <root@Vraj-PC.localdomain>
2019-10-21 03:15:34 -0400

    Updates

---
+++ precache-manifest.01ff731ad400dd88fa1db66622c09f33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc69c22829c6dabfc3523804d042e25e",
+    "url": "/TriviaQuestionSearch/index.html"
+  },
+  {
+    "revision": "5fc4a9f42841d1133d15",
+    "url": "/TriviaQuestionSearch/static/css/main.47f51be9.chunk.css"
+  },
+  {
+    "revision": "fdcb97b2fc09f9be4f6e",
+    "url": "/TriviaQuestionSearch/static/js/2.6fd9af78.chunk.js"
+  },
+  {
+    "revision": "5fc4a9f42841d1133d15",
+    "url": "/TriviaQuestionSearch/static/js/main.917bd6a1.chunk.js"
+  },
+  {
+    "revision": "9426feb68ea7807fc96b",
+    "url": "/TriviaQuestionSearch/static/js/runtime-main.eb341432.js"
+  }
+]);
