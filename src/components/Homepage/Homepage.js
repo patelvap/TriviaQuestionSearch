8faa8875d50901ff1361341fbce9e0f17690f2f0
Vraj Patel <patelvap@gmail.com>
2019-10-23 09:06:18 -0400

    clarified homepage

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -11,7 +11,7 @@ const Homepage = () => {
             <div className="instructions">
                 <h3>How to use this site</h3>
                 <p>On the next page you will have 4 inputs to search by. These inputs allow you to refine your search of the trivia questions by 
-                    the value of the questions (from 100-500 in increments of 100), by the category ID of the questions, and by the earliest or by the latest date
+                    the value of the questions (from 100-500 in increments of 100), by the category ID of the questions (a number between 1 and 18418), and by the earliest or by the latest date
                     a question was shown based on the air date. 
                 </p>
                 <p>Thanks for coming to the site! Click the button below to be taken to start searching!</p>
