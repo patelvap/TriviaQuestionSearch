0d61daa9de93394dcbb2ef9bb57c9e1815a9240f
Vraj Patel <patelvap@gmail.com>
2019-10-26 13:48:50 -0400

    made homepage clearer

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -11,8 +11,9 @@ const Homepage = () => {
             <div className="instructions">
                 <h3>How to use this site</h3>
                 <p>On the next page you will have 4 inputs to search by. These inputs allow you to refine your search of the trivia questions by 
-                    the value of the questions (from 100-500 in increments of 100), by the category ID of the questions (a number between 1 and 18418), and by the earliest or by the latest date
-                    a question was shown based on the air date. 
+                    the value of the questions, by the category ID of the questions (a number between 1 and 18418), and by the earliest or by the latest date
+                    a question was shown based on the air date. The value or difficulty of the questions is in increments of 100 from 100-500 and 600-100 in 
+                    increments of 200 and the date is to be formatted YYYY-MM-DD.
                 </p>
                 <p>Thanks for coming to the site! Click the button below to be taken to start searching!</p>
                 <div className="button"><Link to="/search" style={{ color: '#3393FF', fontSize: 20 }}> Enter </Link></div>
